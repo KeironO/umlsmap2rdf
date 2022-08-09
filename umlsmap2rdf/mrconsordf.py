1f33a8818ca4364d23e24ecf48c419c41c7c65f3
Keiron O'Shea <keiron.oshea@gmail.com>
2022-08-09 09:33:37 +0100

    Update mrconsordf.py

--- umlsmap2rdf/mrconsordf.py
+++ umlsmap2rdf/mrconsordf.py
@@ -18,6 +18,7 @@ class MRCCONSORDF:
         self.out_fp = out_fp
 
 
+    
     def _clean_cui(self, cui: str) -> str:
         return cui.split("\t")[0]
 
@@ -31,8 +32,8 @@ class MRCCONSORDF:
         graph.add((URIRef(prefix.UMLS), URIRef(prefix.RDFS+"comment"), Literal("RDF Version of  UMLS mapping; converted with the UMLSMAPS2RDF tool (https://github.com/KeironO/umlsmap2rdf).")))
         graph.add((URIRef(prefix.UMLS), URIRef(prefix.RDFS+"label"), Literal("UMLS")))
         graph.add((URIRef(prefix.UMLS), URIRef(prefix.OWL+"versionInfo"), Literal("2022AA")))
-
-        with tqdm(total=os.path.getsize(self.fp)) as pbar:
+        file_size = round(os.path.getsize(self.fp)/float(1<<20),3)
+        with tqdm(total=100) as pbar:
             with open(self.fp, "r") as infile:
                 for line in infile:
                     count += 1
@@ -48,7 +49,6 @@ class MRCCONSORDF:
                     for _prefix in prefixes:
                         if _prefix != "UMLS":
                             graph.bind(_prefix, getattr(prefix, _prefix))
-                    
 
                     source = content[11]
                     if source in source_conversion:
@@ -61,8 +61,8 @@ class MRCCONSORDF:
 
                     except AttributeError:
                         pass
-
-                    pbar.update(len(line.encode('utf-8')))
+                    line_size = len(line.encode('utf-8'))/float(1<<20)
+                    pbar.update((line_size/file_size*100))
 
 
         if True in [self.out_fp.endswith(x) for x in ["xml", "rdf"]]:
